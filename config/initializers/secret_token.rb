f137f175b28452933ef76a3fd1b43947b898f770
RailsBridge <railsbridge@railsbridge-VirtualBox.(none)>
2013-02-18 08:21:00 -0600

    initial commit of RailsBridge suggestotron app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '7d2525e24ecc241292a5569f0d76535fb2aff6d0fefa719c07d719331b3d5b93b0d3b2a5d4376e534e0d7d8f92497386a5cb4eb2dbf4dc825a6fe8fe55425bd5'
